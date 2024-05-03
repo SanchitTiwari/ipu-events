4cfebb23dcdfda7a3a41f9fc6befe36e03a60cca
SanchitTiwari <tiwarisanchit47@gmail.com>
2024-05-03 17:26:35 +0530

    updates

--- components/shared/CheckoutButton.tsx
+++ components/shared/CheckoutButton.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 "use client"
 
 import { IEvent } from '@/lib/database/models/event.model'
@@ -37,43 +36,3 @@ const CheckoutButton = ({ event }: { event: IEvent }) => {
 }
 
 export default CheckoutButton
-=======
-// "use client"
-
-// import { IEvent } from '@/lib/database/models/event.model'
-// import { SignedIn, SignedOut, useUser } from '@clerk/nextjs'
-// import Link from 'next/link'
-// import React from 'react'
-// import { Button } from '../ui/button'
-// import Checkout from './Checkout'
-
-// const CheckoutButton = ({ event }: { event: IEvent }) => {
-//   const { user } = useUser();
-//   const userId = user?.publicMetadata.userId as string;
-//   const hasEventFinished = new Date(event.endDateTime) < new Date();
-
-//   return (
-//     <div className="flex items-center gap-3">
-//       {hasEventFinished ? (
-//         <p className="p-2 text-red-400">Sorry, tickets are no longer available.</p>
-//       ): (
-//         <>
-//           <SignedOut>
-//             <Button asChild className="button rounded-full" size="lg">
-//               <Link href="/sign-in">
-//                 Get Tickets
-//               </Link>
-//             </Button>
-//           </SignedOut>
-
-//           <SignedIn>
-//             <Checkout event={event} userId={userId} />
-//           </SignedIn>
-//         </>
-//       )}
-//     </div>
-//   )
-// }
-
-// export default CheckoutButton
->>>>>>> ae11d0a6bcd141dff7adf03c82dfaed6d4057789
